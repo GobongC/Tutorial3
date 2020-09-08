a392c55c7f02cc299b58c0af25757f08185e829b
GobongC <56932388+GobongC@users.noreply.github.com>
2020-09-08 18:46:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tutorial3/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tutorial3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ResourceBundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] items;
+    String[] prices;
+    String[] descriptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items);
+        prices = res.getStringArray(R.array.prices);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
+        myListView.setAdapter(itemAdapter);
+    }
+}
